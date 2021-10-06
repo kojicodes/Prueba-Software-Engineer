b9ee579a66ced21644c3c96252faac683ae377d1
kojicodes <kojisan.zx@gmail.com>
2021-10-06 08:35:10 -0500

    [DELETED] Files re-organized

--- Exercise 3/app/routes/books.router.js
+++
@@ -1,7 +0,0 @@
-const { Router } = require('express');
-const { getUsers, createUser, getUserById, deleteUser, updateUser} = require('../controllers/users');
-
-const router = Router();
-//Routes
-
-module.exports = router;
